cb9764ed35af4a4371ea2bd6a1fdbac6be0501b7
Justina Tautvydaitė <ta.sujte@gmail.com>
2022-10-25 14:44:36 +0300

    final fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,14 +12,18 @@ function Home() {
   return (
     <HomeWrapper>
       <HomeTextWrapper>
-        <H5>SO, YOU WANT TO TRAVEL TO</H5>
-        <H1>SPACE</H1>
-        <P>
-          Let's face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we'll give you a truly out of this world
-          experience!
-        </P>
+        <header>
+          <H5>SO, YOU WANT TO TRAVEL TO</H5>
+          <H1>SPACE</H1>
+        </header>
+        <footer>
+          <P>
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </P>
+        </footer>
       </HomeTextWrapper>
       <BtnWrapper>
         <ButtonHover />
